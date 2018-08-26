cc64072f1a05040eb4332ed596aa6498f0400e95
Annstella <annstellawangui@gmail.com>
2018-08-26 21:07:49 +0300

    add first test

--- user_test.py
+++ user_test.py
@@ -14,3 +14,17 @@ def setUp(self):
     set up method to run before each test cases.
     '''
     self.new_user = User("Annstella","Kagai","annstellawangui12@gmail.com","annstella")
+
+def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly
+    '''
+
+    self.assertEqual(self.new_user.first_name,"Annstella")
+    self.assertEqual(self.new_user.last_name,"Kagai")
+    self.assertEqual(self.new_user.email,"annstellawangui12@gmail.com")
+    self.assertEqual(self.new_user.password,"annstella")
+
+
+if __name__ == '__main__':
+    unittest.main()
