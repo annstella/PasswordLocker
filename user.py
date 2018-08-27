a503b8f685c865d8ef2460b83a3e3e145f77559e
Annstella <annstellawangui@gmail.com>
2018-08-27 08:05:08 +0300

    add info

--- user.py
+++ user.py
@@ -10,7 +10,7 @@ class User:
         save_user method saves user objects into user_list
         '''
         User.user_list.appeng(self)
-    def __init__(self,first_name,last_name,email,password):
+    def __init__(self,first_name,last_name,number,email,password):
         '''
         __init__method that helps us define properties for our objects
 
@@ -19,9 +19,11 @@ class User:
             last_name : New user last name.
             number: New user phone number.
             email : New user email address.
+            password: New user password.
         '''
 
         self.first_name = first_name
         self.last_name = last_name
+        self.number = number
         self.email = email
         self.password = password
