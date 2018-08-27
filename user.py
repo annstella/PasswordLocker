e5a715c88e56aa7b83fe842c28a376a3199fe585
Annstella <annstellawangui@gmail.com>
2018-08-27 09:51:33 +0300

    add info

--- user.py
+++ user.py
@@ -6,7 +6,7 @@ class User:
     user_list = []
 
 
-    def __init__(self,first_name,last_name,number,email,password):
+    def __init__(self,first_name,last_name,email,password):
         '''
         __init__method that helps us define properties for our objects
 
@@ -20,7 +20,6 @@ class User:
 
         self.first_name = first_name
         self.last_name = last_name
-        self.number = number
         self.email = email
         self.password = password
 
