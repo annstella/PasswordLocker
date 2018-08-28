3b391522e59852efe793f01e7860a5ea2e0e3fb8
Annstella <annstellawangui@gmail.com>
2018-08-28 04:38:47 +0300

    add credent test cases

--- user_test.py
+++ user_test.py
@@ -71,5 +71,90 @@ def test_display_all_users(self):
 
     self.assertEqual(User.display_users(),User.user_list)
 
+
+class TestCredent(unittest.TestCase):
+    """
+    Test class that defines test cases for the credent class behaviours
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
+    def setUp(self):
+        """
+        Set up method to run before each test case.
+        """
+        self.new_credent = Credent ("Annstella","kuitwo","ann12k") #create user object
+
+     def tearDown(self):
+        """
+        tearDown method that does the clean-up after each test has run
+        """
+        Credent.credent_list = []
+
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_credent.user_name,"kuitwo")
+
+    def test_save_credent(self):
+        """
+        test to check if the credent object is saved on credent list
+        """
+        self.new_credent.save_credent() #save user
+        self.assertEqual(len(Credent.credent_list),1)
+
+    def test_save_multiple_credent(self):
+        """
+        To check if we can save multiple objects into list
+        """
+        self.new_credent.save_credent()
+        test_credent = Credent ("instagram","clanzym","nutella")
+        test_credent.save_credent()
+        self.assertEqual(len(Credent.credent_list),2)
+
+    def test_delete_credent(self):
+            '''
+            test_delete_credent to test if we can remove credent from our credent list
+            '''
+            self.new_credent.save_credent()
+            test_credent = Credent ("instagram","clanzym","nutella") # new credential
+            test_credent.save_credent()
+
+            self.new_credent.delete_credent()# Deleting a credential object
+            self.assertEqual(len(Credent.credent_list),1)
+
+    def test_find_credent_by_user_name(self):
+        '''
+        test to check if we can find a credent by user name and display information
+        '''
+
+        self.new_credent.save_credent()
+        test_credent = Credent ("instagram","clanzym","nutella") # new credential
+        test_credent.save_credent()
+
+        found_credent = Credent.find_by_user_name("clanzym")
+
+        self.assertEqual(found_credent.pword,test_credent.pword)
+
+    def test_credent_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the credential.
+        '''
+
+        self.new_credent.save_credent()
+        test_credent = Credent ("instagram","clanzym","nutella") # new credential
+        test_credent.save_credent()
+
+        credent_exists = Credent.credent_exist("clanzym")
+
+        self.assertTrue(credent_exists)
+
+    def test_display_all_credent(self):
+        '''
+        method that returns a list of all credent saved
+        '''
+
+        self.assertEqual(Credent.display_credent(),Credent.credent_list)
+
 if __name__ == '__main__':
     unittest.main()
